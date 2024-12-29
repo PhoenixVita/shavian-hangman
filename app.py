eee8c616903510df83e5b2daccfaac5cc75dc383
PhoenixVita <76767052+PhoenixVita@users.noreply.github.com>
2024-12-28 19:18:15 -0500

    fixed filesystem structore to make it compatible with flask

--- app.py
+++ app.py
@@ -2,3 +2,20 @@ from flask import Flask, flash, render_template, request
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+@app.route("/play")
+def play():
+    return render_template("play.html")
+
+
+
+@app.route("/wordbank", methods=["GET", "POST"])
+def wordbank():
+
+    return render_template("wordbank.html")
